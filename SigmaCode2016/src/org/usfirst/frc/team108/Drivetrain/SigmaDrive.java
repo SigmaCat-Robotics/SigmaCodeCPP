163963d1d75ad9c51490045ab0006338df2f2d90
Andrew Gordon <boxerbaby30@gmail.com>
2015-12-24 16:33:12 -0500

    Updates to SigmaEncoder and drive

--- SigmaCode2016/src/org/usfirst/frc/team108/Drivetrain/SigmaDrive.java
+++ SigmaCode2016/src/org/usfirst/frc/team108/Drivetrain/SigmaDrive.java
@@ -1,8 +1,10 @@
 package org.usfirst.frc.team108.Drivetrain;
 
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.VictorSP;
-import org.usfirst.frc.team108.util.*;
+import org.usfirst.frc.team108.util.SigmaEncoder;
 
 public class SigmaDrive {
 
@@ -15,13 +17,16 @@ public class SigmaDrive {
 	VictorSP Right1 = new VictorSP(driveData.RightMotor1);
 	VictorSP Right2 = new VictorSP(driveData.RightMotor2);
 	
+	DoubleSolenoid leftShifter = new DoubleSolenoid(driveData.LeftHighGear, driveData.LeftLowGear);
+	DoubleSolenoid rightShifter = new DoubleSolenoid(driveData.RightHighGear, driveData.RightLowGear);
+	
 	SigmaEncoder leftEncoder = new SigmaEncoder(driveData.LeftEncoderAChannel, driveData.LeftEncoderBChannel,
 												false, true);
 	SigmaEncoder rightEncoder = new SigmaEncoder(driveData.RightEncoderAChannel, driveData.RightEncoderBChannel,
 												true,true);
 	
 	boolean highFront;
-	
+	boolean highGear;
 	double driveSpeed;
 	
 	public SigmaDrive(boolean HighFront) {
@@ -32,6 +37,26 @@ public class SigmaDrive {
 	public double getSpeed(){
 		driveSpeed = ((leftEncoder.getRate()+rightEncoder.getRate())/2)/12;
 		return this.driveSpeed;
+	}
+	
+	public void shiftToLow(){
+		if(highGear){
+			leftShifter.set(Value.kReverse);
+			rightShifter.set(Value.kReverse);
+			highGear = false;
+		}else{
+			highGear = false;
+		}
+	}
+	
+	public void shiftToHigh(){
+		if(highGear){
+			leftShifter.set(Value.kForward);
+			rightShifter.set(Value.kForward);
+			highGear = true;
+		}else{
+			highGear = true;
+		}
 	}
 
 }
