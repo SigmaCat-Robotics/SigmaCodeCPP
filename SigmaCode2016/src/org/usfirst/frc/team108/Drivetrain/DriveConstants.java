e5e4c5a813c61b4a3fccdcb420feeefd9d828f6d
Andrew Gordon <boxerbaby30@gmail.com>
2015-12-24 12:40:59 -0500

    beginning

---
+++ SigmaCode2016/src/org/usfirst/frc/team108/Drivetrain/DriveConstants.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team108.Drivetrain;
+
+public class DriveConstants {
+	
+	public final int LeftMotor1 = 1;
+	public final int LeftMotor2 = 2;
+	public final int RightMotor1 = 3;
+	public final int RightMotor2 = 4;
+	
+	public final int LeftHighGear = 1;
+	public final int LeftLowGear = 2;
+	public final int RightHighGear = 3;
+	public final int RightLowGear = 4;
+	
+	public final int HighGearMaxSpeed = 216;
+	public final int LowGearMaxSpeed = 72;
+
+	public final double DriveWidth = 20.25;
+	public final double DriveLength = 31.3;
+	
+	public final int CyclesPerPulse = 360;
+	
+	public final int LeftEncoderAChannel = 1;
+	public final int LeftEncoderBChannel = 2;
+	
+	public final int RightEncoderAChannel = 3;
+	public final int RightEncoderBChannel = 4;
+	
+	public final boolean FrontWheelHigh = false;
+}
