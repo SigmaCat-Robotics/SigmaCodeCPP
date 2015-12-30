8df5c85efd661a42b9a2eea1fd6743eaa4701189
Andrew Gordon <boxerbaby30@gmail.com>
2015-12-30 15:23:55 -0500

    Get rid of annoying comments

--- SigmaCode2016/src/org/usfirst/frc/team108/robot/Robot.java
+++ SigmaCode2016/src/org/usfirst/frc/team108/robot/Robot.java
@@ -8,22 +8,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team108.Drivetrain.SigmaDrive;
 
-/**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- *
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
- */
+
 public class Robot extends SampleRobot {
     SigmaDrive myRobot;
     Joystick leftStick, rightStick;
@@ -35,16 +20,12 @@ public class Robot extends SampleRobot {
         rightStick = new Joystick(1);
     }
 
-    /**
-     * Drive left & right motors for 2 seconds then stop
-     */
+    
     public void autonomous() {
 
     }
 
-    /**
-     * Runs the motors with tank steering.
-     */
+    
     public void operatorControl() {
 
         while (isOperatorControl() && isEnabled()) {
@@ -54,13 +35,11 @@ public class Robot extends SampleRobot {
             }else{
             	myRobot.shiftToHigh();
             }
-            Timer.delay(0.005);		// wait for a motor update time
+            Timer.delay(0.005);
         }
     }
 
-    /**
-     * Runs during test mode
-     */
+
     public void test() {
     }
 }
