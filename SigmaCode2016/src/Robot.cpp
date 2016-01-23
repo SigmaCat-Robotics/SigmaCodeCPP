48a789914ad76b7312a5b89d347ca43a31b52fd2
gtozzi0 <gtozzi0@gmail.com>
2016-01-23 15:25:51 -0500

    Update Robot.cpp

--- SigmaCode2016/src/Robot.cpp
+++ SigmaCode2016/src/Robot.cpp
@@ -1,53 +1,44 @@
-#include "WPILib.h"
 
-class Robot: public SampleRobot
-{
-	RobotDrive *myRobot; // robot drive system
-	Joystick *lstick,*rstick; // Left and Right Joysticks
-	VictorSP *left1, *left2, *right1, *right2;
 
-public:
-	Robot(){
-		left1 = new VictorSP(1);
-		left2 = new VictorSP(2);
-		right1 = new VictorSP(3);
-		right2 = new VictorSP(4);
-
-		myRobot = new RobotDrive(left1,left2, right1, right2);
-		lstick = new Joystick(0),rstick = new Joystick(1);
-//		myRobot = new RobotDrive();
-		myRobot->SetExpiration(0.1);
-	//	std::thread drive(driveTask);
-	//	drive.detach();
-	}
-
-	void OperatorControl()
-	{
-		driveTask();
-	}
-private:
-	void driveTask(){
-		//myRobot->resetEncoders();
-		while(IsAutonomous() && IsEnabled()){
-			//Auto code here
-		}
-		while(IsOperatorControl() && IsEnabled()){
-			myRobot->TankDrive(lstick->GetY(),rstick->GetY()); // drive with tank style
-			Wait(0.005); // wait for a motor update time
-		}
-	}
-};
-START_ROBOT_CLASS(Robot)
 
 #include "WPILib.h"
 
 Robot::Robot(void)
 {
-	
+  frontLeft  = new VictorSP(1);
+  frontRight = new VictorSP(2);
+  rearLeft   = new VictorSP(3);
+  rearRight  = new VictorSP(4);
+  
+  driveSystem = new RobotDrive(frontLeft, frontRight, rearLeft, rearRight);
+  
+  driveSystem->SetExpiration(0.1);
+  
+  leftStick =  new Joystick(1);
+  rightStick = new Joystick(2);
 }
 
 Robot::~Robot(void)
 {
+  delete frontLeft;
+  delete frontRight;
+  delete readLeft;
+  delete readRight;
+  delete driveSystem;
+  delete leftStick;
+  delete rightStick;
+}
+
+void Robot::RobotMain(void)
+{
+  while(1)
+  {
+    driveSystem->TankDrive(leftStick, rightStick);
+    Wait(0.01);
+    // This doesn't match the example code:
+    //myRobot->TankDrive(lstick->GetY(),rstick->GetY()); // drive with tank style
+    //Wait(0.005); // wait for a motor update time
+  }
 }
 
 
