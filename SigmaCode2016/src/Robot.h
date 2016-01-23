fed536da83fa6aef5f5f1a019f6fa336a1ffd240
gtozzi0 <gtozzi0@gmail.com>
2016-01-23 15:04:41 -0500

    Update Robot.h

--- SigmaCode2016/src/Robot.h
+++ SigmaCode2016/src/Robot.h
@@ -5,9 +5,11 @@ class Robot:public SampleRobot
   public:
     Robot();
     virtual ~Robot();
-	  
-	  void RobotMain(void);
-	  void RobotInit(void);
+    void RobotMain(void);
+    void RobotInit(void);
 
   private:
+    RobotDrive *driveSystem; 			// robot drive system
+    Joystick   *lstick,*rstick; 			// Left and Right Joysticks
+    VictorSP   *left1, *left2, *right1, *right2;
 }
