b2c50b7b5f05bb8fa85e7d1fbb12f89b172204b3
FRC108 <frcteam108@gmail.com>
2016-01-23 13:54:44 -0500

    bare minimum

--- SigmaCode2016/src/Robot.cpp
+++ SigmaCode2016/src/Robot.cpp
@@ -2,30 +2,37 @@
 
 class Robot: public SampleRobot
 {
-	SigmaDrive *myRobot; // robot drive system
+	RobotDrive *myRobot; // robot drive system
 	Joystick *lstick,*rstick; // Left and Right Joysticks
-
+	VictorSP *left1, *left2, *right1, *right2;
 
 public:
 	Robot(){
+		left1 = new VictorSP(1);
+		left2 = new VictorSP(2);
+		right1 = new VictorSP(3);
+		right2 = new VictorSP(4);
+
+		myRobot = new RobotDrive(left1,left2, right1, right2);
 		lstick = new Joystick(0),rstick = new Joystick(1);
-		myRobot = new SigmaDrive();
-		myRobot->setExpiration(0.1);
-		std::thread drive(driveTask);
-		drive.detach();
+//		myRobot = new RobotDrive();
+		myRobot->SetExpiration(0.1);
+	//	std::thread drive(driveTask);
+	//	drive.detach();
 	}
 
 	void OperatorControl()
 	{
+		driveTask();
 	}
 private:
 	void driveTask(){
-		myRobot->resetEncoders();
+		//myRobot->resetEncoders();
 		while(IsAutonomous() && IsEnabled()){
 			//Auto code here
 		}
 		while(IsOperatorControl() && IsEnabled()){
-			myRobot->tankDrive(lstick->GetY(),rstick->GetY()); // drive with tank style
+			myRobot->TankDrive(lstick->GetY(),rstick->GetY()); // drive with tank style
 			Wait(0.005); // wait for a motor update time
 		}
 	}
