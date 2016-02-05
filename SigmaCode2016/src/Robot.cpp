6a73f3d98ad85c12b93a07a5b2e449e87caef7a6
boxerbaby30 <boxerbaby30@gmail.com>
2016-02-05 16:23:34 -0500

    Update Robot.cpp
    For testing chassis

--- SigmaCode2016/src/Robot.cpp
+++ SigmaCode2016/src/Robot.cpp
@@ -4,20 +4,21 @@ class Robot: public SampleRobot
 {
 	RobotDrive *myRobot; // robot drive system
 	Joystick *lstick,*rstick; // Left and Right Joysticks
-	VictorSP *left1, *left2, *right1, *right2;
+	//VictorSP *left1, *left2, *right1, *right2;
 	DoubleSolenoid *shifter;
 
 public:
-	Robot(){
+	void RobotInit() override {
+		/*
 		left1 = new VictorSP(0);
 		left2 = new VictorSP(1);
 		right1 = new VictorSP(2);
 		right2 = new VictorSP(3);
-
-		myRobot = new RobotDrive(left1,left2, right1, right2);
-		lstick = new Joystick(0),rstick = new Joystick(1);
-//		myRobot = new RobotDrive();
-		myRobot->SetExpiration(0.1);
+		*/
+		myRobot = new RobotDrive(1,2,3,4);
+		lstick = new Joystick(0);
+		rstick = new Joystick(1);
+		//myRobot->SetExpiration(0.1);
 	//	std::thread drive(driveTask);
 	//	drive.detach();
 		shifter = new DoubleSolenoid(0,1);
@@ -25,14 +26,6 @@ public:
 
 	void OperatorControl()
 	{
-		driveTask();
-	}
-private:
-	void driveTask(){
-		//myRobot->resetEncoders();
-		while(IsAutonomous() && IsEnabled()){
-			//Auto code here
-		}
 		while(IsOperatorControl() && IsEnabled()){
 			if(lstick->GetRawButton(1) && rstick->GetRawButton(1)){
 				shifter->Set(kForward);
@@ -40,7 +33,7 @@ private:
 			else{
 				shifter->Set(kReverse);
 			}
-			myRobot->TankDrive(lstick->GetY(),rstick->GetY()); // drive with tank style
+			myRobot->TankDrive(lstick,rstick); // drive with tank style
 			Wait(0.005); // wait for a motor update time
 		}
 	}
