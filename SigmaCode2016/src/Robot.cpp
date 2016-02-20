bdaaf3d338d1f8625484ae9846a753096d5143c8
FRC108 <frcteam108@gmail.com>
2016-02-20 12:26:56 -0500

    working

--- SigmaCode2016/src/Robot.cpp
+++ SigmaCode2016/src/Robot.cpp
@@ -1,28 +1,22 @@
 #include "WPILib.h"
 
-RobotDrive *drive;
-Joystick *lstick, *rstick;
-
 /**
  * Uses the CameraServer class to automatically capture video from a USB webcam
  * and send it to the FRC dashboard without doing any vision processing. This
  * is the easiest way to get camera images to the dashboard. Just add this to the
  * RobotInit() method in your program.
  */
-class Robot : public SampleRobot
+class QuickVisionRobot : public SampleRobot
 {
-	DoubleSolenoid *shifter;
+	Joystick *left, *right;
+
 public:
 	void RobotInit() override {
 		CameraServer::GetInstance()->SetQuality(50);
 		//the camera name (ex "cam0") can be found through the roborio web interface
 		CameraServer::GetInstance()->StartAutomaticCapture("cam0");
-		drive = new RobotDrive(1,2,3,4);
-		lstick = new Joystick(0);
-		rstick = new Joystick(1);
-		shifter = new DoubleSolenoid(0,1);
-		//std::thread shift(Shift,shifter,this);
-		//shift.join();
+		left = new Joystick(0);
+		right = new Joystick(1);
 	}
 
 	void OperatorControl()
@@ -30,29 +24,12 @@ public:
 		while (IsOperatorControl() && IsEnabled())
 		{
 			/** robot code here! **/
-			drive->TankDrive(lstick,rstick);
-			if(lstick->GetRawButton(1)){
-				shifter->Set(DoubleSolenoid::kForward);
-			}
-			else{
-				shifter->Set(DoubleSolenoid::kReverse);
-			}
+			SmartDashboard::PutNumber("DB/Slider 0", left->GetY()+1);
+			SmartDashboard::PutNumber("DB/Slider 1", right->GetY()+1);
 			Wait(0.005);				// wait for a motor update time
 		}
 	}
-/*
-	static void Shift(DoubleSolenoid *shift, Robot *bot){
-		while(bot->IsOperatorControl() && bot->IsEnabled()){
-			if(lstick->GetRawButton(1)){
-				shift->Set(DoubleSolenoid::kForward);
-			}
-			else{
-				shift->Set(DoubleSolenoid::kReverse);
-			}
-		}
-	}
-*/
 };
 
-START_ROBOT_CLASS(Robot)
+START_ROBOT_CLASS(QuickVisionRobot)
 
