3224316f57dbf9d35f6392f8e74a53374185a809
FRC108 <frcteam108@gmail.com>
2016-02-22 11:44:37 -0500

    Working w/ Auto (kinda)

--- SigmaCode2016/src/Robot.cpp
+++ SigmaCode2016/src/Robot.cpp
@@ -1,4 +1,5 @@
 #include "WPILib.h"
+//#include "stdio.h"
 
 /**
  * Uses the CameraServer class to automatically capture video from a USB webcam
@@ -16,9 +17,15 @@ class QuickVisionRobot : public SampleRobot
 	DoubleSolenoid *shifter, *shooterAim;
 	Ultrasonic *ballDetect;
 	Task *Operating;
+	Encoder *leftEnc, *rightEnc, *shooterEnc;
+	ADXRS450_Gyro *gyro;
 
 public:
 	void RobotInit() override {
+
+		gyro = new ADXRS450_Gyro();
+		gyro->Calibrate();
+
 		CameraServer::GetInstance()->SetQuality(50);
 		//the camera name (ex "cam0") can be found through the roborio web interface
 		CameraServer::GetInstance()->StartAutomaticCapture("cam0");
@@ -36,6 +43,10 @@ public:
 		rightIndexer = new VictorSP(2);
 		shooter = new VictorSP(1);
 
+		leftEnc = new Encoder(8,9, false, Encoder::EncodingType::k4X);
+		rightEnc = new Encoder(6,7, true, Encoder::EncodingType::k4X);
+		shooterEnc = new Encoder(4,5);
+
 		Lowerlimit = new DigitalInput(0);
 		Upperlimit = new DigitalInput(1);
 
@@ -48,29 +59,88 @@ public:
 		ballDetect->SetEnabled(true);
 		ballDetect->SetAutomaticMode(true); // turns on automatic mode
 
-		QuickVisionRobot *bot = this;
-		Operating = new Task("Operating", Wrapper, bot);
+//		QuickVisionRobot *bot = this;
+//		Operating = new Task("Operating", (FUNCPTR)Wrapper, bot);
+	}
+
+	void Autonomous(){
+
+		int state = 1;
+
+		printf("start!\r\n");
+
+		while(IsAutonomous() && IsEnabled())
+		{
+			switch(state)
+			{
+			case 1:
+				printf("state 1!\r\n");
+				rightEnc->Reset();
+				leftEnc->Reset();
+				shifter->Set(DoubleSolenoid::kForward);
+				drive108->TankDrive(0.7,0.7);
+				state = 2;
+				break;
+			case 2:
+				printf("state 2!\r\n");
+				drive108->TankDrive(0.7,0.7);
+				if (rightEnc->Get() >= 7833)
+				{
+					state = 3;
+				}
+				break;
+			case 3:
+				printf("state 3!\r\n");
+				drive108->TankDrive(0.0,0.0);
+				state = 4;
+				break;
+			case 4:
+				printf("state 4!\r\n");
+				gyro->Reset();
+				drive108->TankDrive(0.7, -0.7);
+				state = 5;
+				break;
+			case 5:
+				printf("state 5!\r\n");
+				drive108->TankDrive(0.7, -0.7);
+				if (gyro->GetAngle() > 45)
+				{
+					state = 6;
+				}
+				break;
+			case 6:
+				break;
+			}
+
+			Wait(.005);
+		}
 	}
 
 	void OperatorControl()
 	{
-	//	int counter = 0;
-	//	bool pulse = true;
-		Operating->join();
+		int counter = 1;
+		int choice = 1;
+		bool pulse = true;
+		char buff[50];
+	//	Operating->join();
 		while (IsOperatorControl() && IsEnabled())
 		{
 			/** robot code here! **/
-			/*if(left->GetRawButton(1) && right->GetRawButton(1)){
-				shifter->Set(DoubleSolenoid::kForward);
-			}else{
-				shifter->Set(DoubleSolenoid::kReverse);
-			}*/
+			if(right->GetRawButton(1)){
+				shifter->Set(DoubleSolenoid::kReverse); // High Gear = reverse
+			}else if(left->GetRawButton(1)){
+				shifter->Set(DoubleSolenoid::kForward); // Low Gear = forward
+			}
 			drive108->TankDrive(left->GetY(),right->GetY());
 			SmartDashboard::PutNumber("DB/Slider 0", left->GetY());
 			SmartDashboard::PutNumber("DB/Slider 1", right->GetY());
-			SmartDashboard::PutNumber("Ball Detect", ballDetect->GetRangeInches());
+
+//			std::sprintf(buff, "%f %f %f\r\n", leftEnc->Get()/20.3, rightEnc->Get()/35.0, shooterEnc->Get());
+			std::sprintf(buff, "%d %d %d\r\n", leftEnc->Get(), rightEnc->Get(), shooterEnc->Get());
+
+			std::printf(buff);
 			Wait(0.005);				// wait for a motor update time
-/*
+
 			if(controller->GetRawAxis(2)>0.2){//down
 				if(!Upperlimit->Get()){
 					armMotor->Set(0.50);
@@ -88,19 +158,63 @@ public:
 				armMotor->Set(0.0);
 			}
 
-			if(controller->GetRawButton(4)){
+			if(controller->GetRawButton(2)){
 				shooterAim->Set(DoubleSolenoid::kForward);
 			}
-			else if(controller->GetRawButton(2)){
+			else if(controller->GetRawButton(4)){
 				shooterAim->Set(DoubleSolenoid::kReverse);
 			}
 
-			if(controller->GetRawButton(5)){//intake
+
+
+			if(controller->GetRawButton(6)){//intake
+				switch(choice){
+					case 1: intake->Set(-0.85);
+							leftIndexer->Set(-0.32108);
+							rightIndexer->Set(0.32108);
+							if(ballDetect->GetRangeInches()<3){
+								choice = 2;
+								counter = 0;
+								pulse = true;
+							}
+							std::printf("CHOICE 1\r\n");
+							break;
+					case 2:	if(counter%64 == 0){
+								leftIndexer->Set(0.0);
+								rightIndexer->Set(0.0);
+								choice = 3;
+							}
+/*
+						if(pulse){
+								leftIndexer->Set(-0.25);
+								rightIndexer->Set(0.25);
+								if(counter%50 == 0){
+									pulse = false;
+								}
+							}else{
+								leftIndexer->Set(0.0);
+								rightIndexer->Set(0.0);
+								if(counter%20 == 0){
+									pulse = true;
+									choice = 3;
+								}
+							}
+*/
+							counter++;
+							//if(ballDetect->Get)
+							std::printf("CHOICE 2\r\n");
+							break;
+					case 3: intake->Set(0.0);
+							//choice = 1;
+							std::printf("CHOICE 3\r\n");
+							break;
+				}
+/*
 				shooter->Set(0);
 				intake->Set(-0.7);
 				if(pulse){
-					leftIndexer->Set(-0.1);
-					rightIndexer->Set(0.1);
+					leftIndexer->Set(-0.25);
+					rightIndexer->Set(0.25);
 					if(counter%20 == 0){
 						pulse = false;
 					}
@@ -111,12 +225,14 @@ public:
 						pulse = true;
 					}
 				}
+*/
 			}
-			else if(controller->GetRawButton(6)){//release
+			else if(controller->GetRawButton(5)){//release
 				shooter->Set(0);
 				intake->Set(0.7);
 				leftIndexer->Set(1.0);
 				rightIndexer->Set(-1.0);
+				choice = 1;
 			}
 			else if(controller->GetRawButton(3)){
 				intake->Set(0.0);
@@ -128,19 +244,22 @@ public:
 					leftIndexer->Set(0.0);
 					rightIndexer->Set(0.0);
 				}
+				choice = 1;
 			}
 			else{//stop
 				shooter->Set(0);
 				intake->Set(0.0);
 				leftIndexer->Set(0.0);
 				rightIndexer->Set(0.0);
+				choice = 1;
 			}
-			counter = counter+1;*/
 		}
 	}
 
+	/*
 private:
 	void ShootIntake(){
+		SmartDashboard::PutString("Hi", "I'm Bob");
 		int counter = 0;
 		bool pulse = true;
 		while(IsOperatorControl() && IsEnabled()){
@@ -215,6 +334,7 @@ private:
 	static void Wrapper(QuickVisionRobot *bot){
 		bot->ShootIntake();
 	}
+	*/
 };
 
 START_ROBOT_CLASS(QuickVisionRobot)
